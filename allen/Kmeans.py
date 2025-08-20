56c01ca1a29579ce9d7405c624d796f265650de0
allen liu <allen@allens-Air.attlocal.net>
2025-08-20 00:01:26 -0700

    add K means implementation

---
+++ allen/Kmeans.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# K means
+# 1. init n data points
+# 2. randomly init K centroids
+# 3. each data point find closest centroid to go
+# 4. update centoids based on groups
+# 5. loop over 3 and 4
+
+K = 4
+
+data = np.random.randn(100, 2)
+
+centroids = np.random.randn(K, 2)
+
+
+groups = {k: [] for k in range(K)}
+
+for i in range(10):
+    for dp in data:
+        dists = []
+        for cent in centroids:
+            dist = np.sqrt(np.sum((dp - cent) ** 2))
+            dists.append(dist)
+        groups[dists.index(min(dists))].append(dp)
+
+    # calculate centroids
+    new_centroids = []
+    for key in groups:
+        new_centroid = np.mean(groups[key], axis=0)
+        new_centroids.append(new_centroid)
+
+    centroids = new_centroids
+print(groups)
+        
